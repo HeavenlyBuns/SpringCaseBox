5a1adb34610e172fe8007a16f618bfefea692718
she.haoran@yanwei365.com <she1995she>
2020-01-19 14:55:12 +0800

    add: 项目初始化配置

---
+++ spring-security/src/main/java/com/heavenlybuns/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.heavenlybuns.springsecurity.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author Administrator
+ * @title: Shr
+ * @description: TODO
+ * @date 2020/1/19 14:53
+ */
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        
+        super.configure(auth);
+    }
+}
