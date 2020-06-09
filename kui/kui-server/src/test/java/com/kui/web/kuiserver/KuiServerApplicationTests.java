954deffa64a0e8287709d5f307c64f673b70f2f0
Administrator <435105706@qq.com>
2020-06-10 00:01:51 +0800

    fix

---
+++ kui/kui-server/src/test/java/com/kui/web/kuiserver/KuiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kui.web.kuiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
